22066e67a14f1523d66a38481cd4b2f09f7b9d1d
llc <619593196@qq.com>
2024-08-29 23:56:13 +0800

    feat: 引入拖拽组件

--- src/pages/Panel/index.js
+++ src/pages/Panel/index.js
@@ -1,8 +1,23 @@
+import { DragDropContext } from "react-beautiful-dnd"
 function Index() {
+    const onDragStart = () => {
+      };
+      const onDragUpdate = () => {
+      }
+      const onDragEnd = () => {
+      }
     return (
         <div>
             kanban
-            
+            <div className="dragable-container">
+                <DragDropContext
+                    onDragStart={onDragStart}
+                    onDragUpdate={onDragUpdate}
+                    onDragEnd={onDragEnd}
+                >
+                    <div>Hello world</div>
+                </DragDropContext>
+            </div>
         </div>
     )
 }
